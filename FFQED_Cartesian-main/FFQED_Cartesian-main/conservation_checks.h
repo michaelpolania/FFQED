83f1646f767401d870aa27d1f7b0c03ac15a5e83
michaelpolania <michael.polania14@login.cuny.edu>
2026-03-05 22:41:40 -0500

    Delete FFQED_Cartesian-main directory

--- FFQED_Cartesian-main/FFQED_Cartesian-main/conservation_checks.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONSERVATION_CHECKS_H_INCLUDED
-#define CONSERVATION_CHECKS_H_INCLUDED
-
-double divB_Calculator(VectorField & B, size_t N_GC);
-void divB_Monitor(VectorField & B, size_t N_GC,  MPI_Comm comm1D, int world_rank, std::vector<int> & Ny_locs);
-void EnergyConservation(VectorField & B, VectorField & E, VectorField & J, ScalarField & eta_O, size_t N_GC, double t, const Domain & dm, const BandBCParams & bparams, double & U_B, double & JouleH, double & PoyntingF);
-
-#endif // CONSERVATION_CHECKS_H_INCLUDED
--- FFQED_Cartesian-main/FFQED_Cartesian-main/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_csv('TOVBSk24Crust.dat')
-print(df.head())
-print(len(df))
